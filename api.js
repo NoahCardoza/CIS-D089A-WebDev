e6fe921ac67c51068d91f13e5e5793aca0fffab8
Noah Cardoza <noahcardoza@Darwin.local>
2017-10-23 17:38:10 -0700

    made good progress on the terminal

---
+++ api.js
@@ -0,0 +1,30 @@
+var API = function(endpoint, data){
+	return new Promise( function( resolve, reject ) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', "http://battleofthe.net/api/"+endpoint+".php", true); // this will have to chage
+		xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+		xhr.setRequestHeader("X-lax-HTTP", "1.4.05");
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.onload = function () {
+			if (this.status >= 200 && this.status < 300) {
+				resolve(JSON.parse(xhr.response));
+			} else {
+				alert("HTTP Error: "+this.status);
+				console.error(endpoint, this.status, xhr.statusText);
+				reject({
+					status: this.status,
+					statusText: xhr.statusText
+				});
+			}
+		};
+		xhr.onerror = function () {
+			alert("HTTP Error: "+this.status);
+			console.error(endpoint, this.status, xhr.statusText);
+			reject({
+				status: this.status,
+				statusText: xhr.statusText
+			});
+		};
+		xhr.send(+JSON.stringify(data));
+	});
+};
