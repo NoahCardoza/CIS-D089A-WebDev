1965f6046a56be83cb8c4225c35df1f33212b625
Noah Cardoza <noahcardoza@gmail.com>
2017-11-09 21:51:43 -0800

    CHANGE

--- index.php
+++ index.php
@@ -58,12 +58,12 @@
 			<iframe style="width: 100%; height: 40vh" src="https://discordapp.com/widget?id=360140003180806145&theme=dark" allowtransparency="true" frameborder="0"></iframe>
 		</div>	
 		<div class="block-msg-md">
-			<span class="anchor" id="about"></span>
+			<span class="anchor" name="about"></span>
 			<h2 class="block-center">About</h2>
 			<p><b>Battle of the Net</b> is a game I and a friend came up with. The goal of the game is to <i>hack</i> other players, gain money, to  reputation in the hacker underworld.<br>When all is done we plan to feature an in-game <b>Terminal</b> where most of the game will be played. While quite unrealistic, we aim to bring the most practical experience yet. We hope that this game will educate its players teaching them the basics of the <b>UNIX OS</b> and dangers of malicious users.</p>
 		</div>
 		<div class="block-msg-md">
-			<span class="anchor" id="updates"></span>
+			<span class="anchor" name="updates"></span>
 			<h2 class="block-center">Updates</h2>
 			<article class="block-update">
 				<div class="block-title"><span class="lable-success float-right">10-21-17</span><h3>Server Back Online</h3></div>
@@ -84,7 +84,7 @@
 			</article>
 		</div>
 		<div class="block-msg-md">
-			<span class="anchor" id="contact"></span>
+			<span class="anchor" name="contact"></span>
 			<h2 class="block-center">Contact</h2>
 			<p>If you have any concerns, comments, or death threats, those go here.</p>
 			<form id="userinput" style="width: 100%;">
